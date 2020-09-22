79e783defe50651e4ec3bbeee56d038cb187774c
golimathur <39324030+golimathur@users.noreply.github.com>
2020-09-22 19:47:49 +0530

    Update Validate.tsx

--- src/login/Validate.tsx
+++ src/login/Validate.tsx
@@ -1,97 +1,82 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Link } from "react-router-dom";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  setNestedObjectValues,
+} from "formik";
+import axios from "axios";
 import "./login.css";
-import Axios from "axios";
+import history from "../history";
 
-// interface IUser {
-//   email: String;
-//   password: any;
-// }
-const handleSubmit = (e: any) => {
-  // e.preventDefault();
-  const url = "https://apiuat.actingoffice.com/Auth/SignIn";
-
-  const userObject = {
-    userName: e.userName || "",
-    password: e.password || "",
-  };
-
-  Axios.post(url, userObject)
-    .then(function (response) {
-      console.log("logged in");
-      console.log(response);
-    })
-
-    .catch(function (error) {
-      console.log(error);
-    });
-};
-
-// function validateEmail(value) {
-//   let error;
-
-//   if (!value) {
-//     error = "Required";
-//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
-//     error = "Invalid email address";
-//   }
-
-//   return error;
-// }
-
-// function validatePassword(value) {
-//   let error;
-
-//   if (value === "admin") {
-//     error = "Nice try!";
-//   }
-
-//   return error;
-// }
-
-const Validate = () => {
-  const onSubmit = (values) => console.log(values);
-
+const Validate = (props: any) => {
+  const [values, setValues] = React.useState({});
   return (
-    <div style={{ borderWidth: 10, borderColor: "#0078d4", border: 3 }}>
+    <div>
       <Formik
         initialValues={{
-          email: "",
+          userName: "",
           password: "",
         }}
-        onSubmit={onSubmit}
+        onSubmit={async (values) => setValues(values)}
       >
-        {({ errors, touched }) => (
-          <Form
-            onSubmit={(e: any) => {
-              e.preventDefault();
-              handleSubmit(e);
-            }}
-          >
-            {/* <p>Email</p>
-            <Field name="email" validate={validateEmail} />
-            {errors.email && touched.email && errors.email}
-            <p>Password</p>
-            <Field
-              type="password"
-              name="password"
-              validate={validatePassword}
-            />
-            {errors.password && touched.password && errors.password} */}
+        {(props) => {
+          const { values, isSubmitting, handleChange } = props;
 
-            <label htmlFor="userName">Email / UserName:</label>
-            <Field id="userName" type="email" name="userName" />
-            <ErrorMessage name="userName" className="errorMsg" component="p" />
-            <label htmlFor="password">password:</label>
-            <Field id="password" name="password" component="textarea" />
-            <ErrorMessage name="password" className="errorMsg" component="p" />
+          return (
+            <Form
+              onSubmit={() =>
+                axios
+                  .post("https://apiuat.actingoffice.com/Auth/SignIn", values)
 
-            <button type="submit" onClick={(e: any) => handleSubmit(e)}>
-              Submit
-            </button>
-          </Form>
-        )}
+                  .then(function (response) {
+                    console.log(response);
+                    const history :any[]= [];
+                    history.push("http://localhost:3000/Dashboard");
+                    if (response) {
+                    } else alert(JSON.stringify(response));
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  })
+              }
+            >
+              <label htmlFor="userName">Email </label>
+              <input
+                placeholder="Email address"
+                type="userName"
+                name="userName"
+                value={values.userName}
+                onChange={handleChange}
+                required
+              />
+              <ErrorMessage
+                name="userName"
+                className="errorMsg"
+                component="p"
+              />
+              <label htmlFor="password">password:</label>
+              <input
+                placeholder="Password"
+                type="password"
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                required
+              />
+              <ErrorMessage
+                name="password"
+                className="errorMsg"
+                component="p"
+              />
+
+              <button disabled={isSubmitting} type="submit">
+                Submit
+              </button>
+            </Form>
+          );
+        }}
       </Formik>
     </div>
   );
